eb9ad5166cbde34f7f562c3e2cbe9971c71ee07f
Koder741 <47461674+Koder741@users.noreply.github.com>
2019-07-27 18:31:11 -0600

    Update AdventureGameText.java

--- AdventureGameText.java
+++ AdventureGameText.java
@@ -1,4 +1,4 @@
-package text;
+package text;	//current package
 
 import gui.*;	//future package
 import logic.*; //future package
@@ -7,59 +7,76 @@ import java.util.Random;
 import java.util.Scanner;
 
 
+
 /** 
  * Class AdventureGameText controls the entire application.
  * It creates one instance for Map and Player called map and playerInfo, respectively. 
- * Both the map and player location will be updated with every game iteration. 
- * 
  */
 public class AdventureGameText  {
 
 	private Map map;
-	private Player playerInfo; //
+	private Player player; 
 	
+	/*
+	 * Default constructor - takes no arguments
+	 * @param None 
+	 */
+	public AdventureGameText() {}
 	
 	/**
-	 * Method placeCollectible creates a randomly-generated x and y coordinates for items
-	 * in the Collectible class (e.g. notes, chests and keys). It uses a constructor from Location 
-	 * to place said items.
+	 * Constructor takes references from Map and Player and assigns them to the instance variables
+	 * @param aMap and aPlayer a map of type Map and a player of type Player
 	 */
-	public void placeCollectible(Collectible item) {
-
-		int x = (int)Math.random();
-		int y = (int)Math.random();
-		Location coordinates = new Location(item, x, y); //update class diagram to add this constructor 
+	public AdventureGameText(Map aMap, Player aPlayer) {
+		map = aMap;
+		player = aPlayerl
+	}
+	
+	/**
+	 * Copy constructor that copies the values an existing object 
+	 * @param toCopy an existing object
+	 */
+	public AdventureGameText(AdventureGameText toCopy) {
+		map = toCopy.map;
+		player = toCopy.player;
 	}
 	
 	/** 
-	 * Method play interacts with the user upon the creation of a new game and asks for a move,
-	 * which is then translated into a shift in the x and y axis. It also asks the user if it wants to use a collectible item,
-	 * which then invokes the appropriate method in the Collectible class. 
+	 * Method play interacts with the user upon the creation of a new game and asks it for a move,
+	 * which is then translated into a shift in the x and y axis. 
 	 */
-	
 	public void play() {	
 	
 		displayGrid(); 
 		
-		System.out.println("Enter your first move ('a' for left, 'd' for right, 'w' for up, 's' for down, 'd' to quit: ");
 		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter your move: ");
 		char ch = kb.nextLine();
-		map.move(ch);
-
-	/*
+		
+		while (ch != q) {
+			map.move(ch);
+			System.out.println("Enter your move: ");
+			char ch = kb.nextLine();
+		}
+		System.out.println("Invalid Move.");
+	}
+	
+	/**
 	 * Method displays a functioning menu at the beginning of the app. It asks the user to run the game or exit,
 	 * the later which restarts the opening message.
 	 */
-	publci void displayMenu() {
-	  //Opening Message and Instructions
-		System.out.println("\t *Welcome to The Entity*");
-		System.out.println("\t*********************************");
-		System.out.println("\nYou are stuck in a house, and must get out");
-		System.out.println("You are shown using the 'X' character. Good luck!");
+	public void mainMenu() {
+		//Opening Message and Instructions
+		System.out.println("\n\t             <<Welcome to The Entity>>");
+		System.out.println("\t         *********************************");
+		System.out.println("\n");	
+		System.out.println("\t   You are stuck in a house, and must get out");
+		System.out.println("\tYou are shown using the 'X' character. Good luck!");
 		System.out.println("\n");		
-		System.out.println("Press 'r' to run game or press 'q' to quit");
-		System.out.println("Press 'a' to move left, or press 'd' to move right. ");
-		System.out.println("\t******************************");
+		System.out.println("\t   Press 'r' to run game or press 'q' to quit");
+		System.out.println("\t 'a' = left, 'd' = right, 'w' = up, 's' = down");
+		System.out.println("\n");	
+		System.out.println("\t         *********************************");
 		System.out.println("\n");
 		
 		Scanner kb = new Scanner(System.in);
@@ -72,9 +89,22 @@ public class AdventureGameText  {
 		}
 		
 	}
+		
+	public static void main(String[] args) {
+		Map map = new Map();
+		Player player = new Player();
+	}
 	
-	
-    public static void main(String[] args) {
-    //Launches app
- }
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
