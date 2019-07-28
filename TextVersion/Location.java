fa6bf7f5154864aa21c7bbb3e49c42cf80637598
Koder741 <47461674+Koder741@users.noreply.github.com>
2019-07-28 17:12:27 -0600

    Location

---
+++ TextVersion/Location.java
@@ -0,0 +1,69 @@
+/**
+ * Class Location indicates the location in the map. It contains an instance for row and column.
+ */
+public class Location {
+
+	private int row;
+	private int col;
+	
+	/*
+	 * Default constructor - takes no arguments
+	 * @param None 
+	 */
+	public Location() {}
+	/**
+	
+	 * Constructor takes an argument for row and column and assigns them to the instance variables
+	 * @param aRow, Col a row and a column 
+	 */
+	public Location(int aRow, int aCol) {
+		row = aRow;
+		col = aCol;
+	}
+	
+	/**
+	 * Copy constructor that copies the values an existing object 
+	 * @param toCopy an existing object
+	 */
+	public Location(Location toCopy) {
+		row = toCopy.row;
+		col = toCopy.col;
+	}
+	
+	/**
+	 * Gets the current row
+	 * @param None
+	 */
+	public int getRow() {
+		return row;
+	}
+	
+	/**
+	 * Gets the current column
+	 * @param None
+	 */
+	public int getCol() {
+		return col;
+	}
+	
+	/**
+	 * Sets the current row
+	 * @param aRow a row 
+	 */
+	public void setRow(int aRow) {
+		if (aRow <= 10) {
+			row = aRow;
+		}
+	}
+	
+	/**
+	 * Sets the current column
+	 * @param aCol a column 
+	 */
+	public void setCol(int aCol) {
+		if (aCol <= 50) {
+			col = aCol;
+		}
+	}
+	
+}
