4267a96b4a49f5d0fb59512cc12dc19f6ba12cdc
Koder741 <47461674+Koder741@users.noreply.github.com>
2019-08-03 15:48:40 -0600

    Player.java

---
+++ TextVersion/Player.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+ 
+/**
+ * Class Player contains the code for the character that the user controls and moves around 
+ * the screen. It contains information such as items picked up and the character's current 
+ * location in the map. 
+ */
+public class Player {
+
+	private char player = 'X';
+	private ArrayList<Collectible> item;
+	private Direction direction;
+	private Location location = new [0][0];
+	
+	/*
+	 * Default constructor - takes no arguments
+	 * @param None 
+	 */
+	public Player() {}
+	
+	/**
+	 * Constructor takes arguments for item, character, and location assigns them to the instance variables
+	 * @param anItem an item from the Collectible class
+	 * @param aPlayer a player that represents the user
+	 * @param aLocation the current location
+	 */
+	public Player(ArrayList<Collectible> anItem, char aPlayer, Location startLocation) { //UPDATE
+		item = anItem;
+		character = aCharacter;
+		location = startLocation;
+	}
+	
+	/**
+	 * Copy constructor that copies the values an existing object 
+	 * @param toCopy a copy of an existing object
+	 */
+	public Player(Player toCopy) { //UPDATE
+		item = toCopy.item;
+		character = toCopy.character;
+		location = toCopy.location;
+	}
+	
+	
+	public Direction getDirection() {
+		return direction;
+	}
+	public void setDirection(Direction aDirection) {
+		direction = aDirection;
+	}
+	
+	public void itemsPicked() {}
+		// TO DO
+	
+	public Location getLocation() { //player's current location
+		return location;
+	}
+	public void setLocation(Location aLocation) {
+		location = aLocation;
+	}
+	
+	public void pickUpItem(Collectible item) {}	//TO DO
+	public void readNote(Collectible note) {}	//TO DO 
+	public void useItem(Inventory itemToUse) {}	//TO DO
+	public void openChest(Collectible chest) {}	// TO DO
+	
+}
