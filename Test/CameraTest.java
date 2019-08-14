9a6ce6e365cc13d6a731cfbc4aa403dfe675179e
BoBajwa93 <52938674+BoBajwa93@users.noreply.github.com>
2019-08-14 03:26:53 -0600

    Add files via upload

---
+++ Test/CameraTest.java
@@ -0,0 +1,26 @@
+package main.gameclasses;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CameraTest {
+
+	@Test
+	public void test() {
+	Camera test = new Camera(0, 0);
+	assertEquals(1050, 1050);
+	//test for camera bound x, if its = to 1050
+	
+
+		
+	
+}
+	@Test
+	public void test2() {
+	Camera test = new Camera(0, 0);
+	assertEquals(1320, 1320);
+	//test for camera bound y if its = 1320
+	
+}
+}
